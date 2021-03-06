2a42d77c3ff3f9dfd3d68d81cb5c4658d36d04d5
RSN <16201554+ridoansaleh@users.noreply.github.com>
2021-03-06 17:00:23 +0700

    add new questions (#4)

--- src/routes/questions/data.js
+++ src/routes/questions/data.js
@@ -3,6 +3,11 @@ import image_02 from "./images/kantor-bupati-nias.jpg";
 import image_03 from "./images/tor-sibohi.jpg";
 import image_04 from "./images/makam-papan-tinggi.jpg";
 import image_05 from "./images/tarutung.jpg";
+import image_06 from "./images/jalan-laguboti.jpg";
+import image_07 from "./images/padang-halaban.png";
+import image_08 from "./images/puncak-galau.jpg";
+import image_09 from "./images/kebun-teh-bah-butong-sidamanik.jpg";
+import image_10 from "./images/pemandian-lau-timah.jpg";
 
 export const questions = [
   {
@@ -14,31 +19,66 @@ export const questions = [
   },
   {
     id: 2,
-    question: "Apakah Ibu Kota Kabupaten Nias?",
+    question: "Apakah nama Ibu Kota Kabupaten Nias?",
     answers: ["Gido", "Sirombu", "Afulu", "Lahewa"],
     image: image_02,
     image_source: "https://blog.showus.biz",
   },
   {
     id: 3,
-    question: "Apakah Ibu Kota Kabupaten Tapanuli Selatan?",
+    question: "Apakah nama Ibu Kota Kabupaten Tapanuli Selatan?",
     answers: ["Pintu Padang", "Aek Badak", "Sipirok", "Batang Toru"],
     image: image_03,
     image_source: "https://www.pariwisatasumut.net",
   },
   {
     id: 4,
-    question: "Apakah Ibu Kota Kabupaten Tapanuli Tengah?",
+    question: "Apakah nama Ibu Kota Kabupaten Tapanuli Tengah?",
     answers: ["Barus", "Pandan", "Sorkam", "Sibolga"],
     image: image_04,
     image_source: "https://phinemo.com",
   },
   {
     id: 5,
-    question: "Apakah Ibu Kota Kabupaten Tapanuli Utara?",
+    question: "Apakah nama Ibu Kota Kabupaten Tapanuli Utara?",
     answers: ["Tarutung", "Situmeang", "Siborongborong", "Balige"],
     image: image_05,
     image_source: "https://www.sewukuto.id",
+  },
+  {
+    id: 6,
+    question: "Apakah nama Ibu Kota Kabupaten Toba Samosir?",
+    answers: ["Laguboti", "Balige", "Porsea", "Pangururan"],
+    image: image_06,
+    image_source: "https://id.wikipedia.org",
+  },
+  {
+    id: 7,
+    question: "Apakah nama Ibu Kota Kabupaten Labuhanbatu?",
+    answers: ["Kualamerbau", "Perbaungan", "Tanjungmedan", "Rantau Prapat"],
+    image: image_07,
+    image_source: "https://wisataterbaru.net",
+  },
+  {
+    id: 8,
+    question: "Apakah nama Ibu Kota Kabupaten Asahan?",
+    answers: ["Tanjung Balai", "Simpangampat", "Kisaran", "Ulakmedan"],
+    image: image_08,
+    image_source: "https://www.itrip.id",
+  },
+  {
+    id: 9,
+    question: "Apakah nama Ibu Kota Kabupaten Simalungun?",
+    answers: ["Pematang Siantar", "Sarimatondang", "Dalandolok", "Raya"],
+    image: image_09,
+    image_source: "https://www.itrip.id",
+  },
+  {
+    id: 10,
+    question: "Apakah nama Ibu Kota Kabupaten Dairi?",
+    answers: ["Sidikalang", "Berastagi", "Sigalingging", "Parendean"],
+    image: image_10,
+    image_source: "https://www.andalastourism.com",
   },
 ];
 
@@ -62,5 +102,25 @@ export const answers = [
   {
     id: 5,
     answer: "Tarutung",
+  },
+  {
+    id: 6,
+    answer: "Balige",
+  },
+  {
+    id: 7,
+    answer: "Rantau Prapat",
+  },
+  {
+    id: 8,
+    answer: "Kisaran",
+  },
+  {
+    id: 9,
+    answer: "Raya",
+  },
+  {
+    id: 10,
+    answer: "Sidikalang",
   },
 ];
