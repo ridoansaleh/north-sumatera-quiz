944b6e2b993444a63efce309e6d661b5aac33417
RSN <16201554+ridoansaleh@users.noreply.github.com>
2021-03-13 04:32:31 +0700

    add social media sharing buttons (#9)

--- src/fb_event.js
+++ src/fb_event.js
@@ -1,33 +1,37 @@
 const { NODE_ENV } = process.env;
 
 export const initFbSDK = () => {
-  return new Promise((resolve) => {
-    if (NODE_ENV !== "production") {
-      resolve({
-        message: "You're in development. Facebook Analytic is disabled.",
-      });
-    }
-    window.fbAsyncInit = function () {
-      window.FB.init({
-        appId: "1051251482023398",
-        cookie: true,
-        xfbml: true,
-        version: "v10.0",
-      });
-      window.FB.AppEvents.logPageView();
-    };
-    (function (d, s, id) {
-      var js,
-        fjs = d.getElementsByTagName(s)[0];
-      if (d.getElementById(id)) {
-        return;
+  return new Promise((resolve, reject) => {
+    try {
+      if (NODE_ENV !== "production") {
+        resolve({
+          message: "You're in development. Facebook Analytic is disabled.",
+        });
       }
-      js = d.createElement(s);
-      js.id = id;
-      js.src = "https://connect.facebook.net/en_US/sdk.js";
-      fjs.parentNode.insertBefore(js, fjs);
-    })(document, "script", "facebook-jssdk");
-    resolve({ message: "Facebook SDK has been initialized." });
+      window.fbAsyncInit = function () {
+        window.FB.init({
+          appId: "1051251482023398",
+          cookie: true,
+          xfbml: true,
+          version: "v10.0",
+        });
+        window.FB.AppEvents.logPageView();
+      };
+      (function (d, s, id) {
+        var js,
+          fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) {
+          return;
+        }
+        js = d.createElement(s);
+        js.id = id;
+        js.src = "https://connect.facebook.net/en_US/sdk.js";
+        fjs.parentNode.insertBefore(js, fjs);
+      })(document, "script", "facebook-jssdk");
+      resolve({ message: "Facebook SDK has been initialized." });
+    } catch (err) {
+      reject(err);
+    }
   });
 };
 
--- src/routes/finish/index.jsx
+++ src/routes/finish/index.jsx
@@ -1,6 +1,14 @@
 import { useHistory } from "react-router-dom";
 import { Icon, Button } from "semantic-ui-react";
-import { Container, Warning } from "./_finishStyle";
+import {
+  FacebookShareButton,
+  FacebookIcon,
+  TwitterShareButton,
+  TwitterIcon,
+  WhatsappShareButton,
+  WhatsappIcon,
+} from "react-share";
+import { Container, Warning, ShareContainer } from "./_finishStyle";
 import { logFbEvent } from "../../fb_event";
 import {
   APP_PATHS,
@@ -52,11 +60,34 @@ function Finish() {
         onClick={handleLihatReviewClick}
         content="Lihat Review"
       />
-      {quizScore < MIN_PASS_QUIZ_SCORE && (
+      {quizScore < MIN_PASS_QUIZ_SCORE ? (
         <Warning>
           <h4>Kelihatannya nilai kamu tidak cukup bagus</h4>
           <Button primary onClick={handleCobaLagiClick} content="Coba Lagi" />
         </Warning>
+      ) : (
+        <ShareContainer>
+          <FacebookShareButton
+            quote={`Saya baru saja memperoleh nilai ${quizScore} dengan mengerjakan kuis Sumut dalam waktu ${quizTime}.`}
+            hashtag="#kuis"
+            url="https://kuis-sumut.netlify.app"
+          >
+            <FacebookIcon size={32} />
+          </FacebookShareButton>
+          <TwitterShareButton
+            title={`Saya baru saja memperoleh nilai ${quizScore} dengan mengerjakan kuis Sumut dalam waktu ${quizTime}.`}
+            hashtags={["kuis", "sumut", "sumatera", "utara", "senang"]}
+            url="https://kuis-sumut.netlify.app"
+          >
+            <TwitterIcon size={32} />
+          </TwitterShareButton>
+          <WhatsappShareButton
+            title={`Saya baru saja memperoleh nilai ${quizScore} dengan mengerjakan kuis Sumut dalam waktu ${quizTime}.`}
+            url="https://kuis-sumut.netlify.app"
+          >
+            <WhatsappIcon size={32} />
+          </WhatsappShareButton>
+        </ShareContainer>
       )}
     </Container>
   );
